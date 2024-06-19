e5ab7a5494dcb049a2dc88c94e2cbe9142ba0025
FRONT-END.03 <FRONT-END.03@TURBOTECH.COM>
2024-06-19 10:54:21 +0700

    commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.master')
+@section('content')
+<h1 class=" text-red-500 text-center">Home</h1>
+@endsection 
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,12 @@
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initail-scale=1.0">
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        @yield('content')
+    </body>
+    <footer>
+    </footer>
+</html>
